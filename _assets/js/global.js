1bc3d4b37fa4ce4db40629052ff0689df177178b
Aaron Alexander <nerdfiles@gmail.com>
2011-11-06 19:37:50 -0600

    cleanup

--- _assets/js/global.js
+++ _assets/js/global.js
@@ -1,1 +1,33 @@
 "use strict";
+
+$(function() {
+
+	$('#wat button').bind('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		var socket = io.connect('http://localhost:3000');
+		var $this = $(this).closest('form');
+
+		//console.log($this.serialize());
+
+		socket.on('connect', function (data) {
+			var data = 'test form socket',
+					anothervalue = (64*200);
+
+			//console.log(data);
+
+			// @note data sanitize? calculations, etc.
+
+			// @note pass data to socket to store
+			
+			socket.emit('test form', { 
+				my: data, 
+				anothervalue: anothervalue, 
+				form: $this.serialize() 
+			});
+		});
+
+	});
+
+});
--- express.js
+++ express.js
@@ -4,6 +4,7 @@
 
 var config = {};
 
+config['host'] =						'localhost';
 config['address'] =         '127.0.0.1';
 config['frontend_port'] =   3000;
 config['backend_port'] =    27017;
@@ -11,30 +12,73 @@ config['backend_port'] =    27017;
 
 /* == REQUIRES =================================== */
 
+/* node */
+
 var Sys = require( 'sys' ),
     Url = require( 'url' ),
     Path = require( 'path' ),
     Fs = require( 'fs' );
     
-/* Jade */
+/* jade */
 
 var Jade = require( 'jade' );
 
-/* Express */
+/* express */
 
 var Express = require( 'express' ),
     App = Express.createServer();
+
+/* express third-party */
+
+var Resourse = require( 'express-resource' ),
+    Expose = require('express-expose');
     
-/* MongoDB */
+/* mongodb */
 
 var Db = require( 'mongodb' ).Db,
     Server = require( 'mongodb' ).Server,
-    DbServer = new Db( 'local', new Server( config["address"], config["backend_port"], {} ) );
+    DbServer = new Db( 'journal', new Server( config["address"], config["backend_port"], {} ) );
+
+/* request */
+
+var Request = require( 'request' );
+
+Request('http://www.google.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    //console.log(body) // Print the google web page.
+  }
+});
+
+/* optimist */
+
+var Optimist = require( 'optimist' );
+
+/* colors */
+
+var Colors = require( 'colors' );
+
+/* socket.io */
+
+var Io = require( 'socket.io' ).listen(App);
 
 
-/* == START =================================== */
+/* == CONFIG =================================== */
 
-/* Config & Environments */
+function handler(request, response) {
+  Fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      response.writeHead(500);
+      return response.end('Error loading index.html');
+    }
+
+    response.writeHead(200);
+    response.end(data);
+  });
+}
+
+
+/* == CONFIG =================================== */
 
 App.configure(function(){
 
@@ -48,8 +92,14 @@ App.configure(function(){
   App.use(Express.bodyParser());
   App.use(Express.static(__dirname + '/_assets'));
   App.use(Express.errorHandler({ dumpExceptions: true, showStack: true }));
-  App.use(require('stylus').middleware({ src: __dirname + '/_assets' }));
-  
+  App.use(require('stylus').middleware({ src: __dirname + '/_assets', compile: compile }));
+
+  function compile (str, path) {
+    return stylus(str)
+      .set('filename', path)
+      .use(nib());
+  };
+
   App.use(App.router);
   
 });
@@ -62,27 +112,76 @@ App.configure('production', function(){
   App.use(Express.errorHandler());
 });
 
-/* Express */
+
+/* == VIEWS =================================== */
 
 App.get('/', function(request, response) {
   
   var uri = Url.parse(request.url).pathname;
   
   response.render('index', { 
-    title: 'Node Test'
+    title: 'Node Test',
   });
   
 });
 
-App.listen( config["frontend_port"] );
+App.get('/test', function(request, response) {
+  
+  var uri = Url.parse(request.url).pathname;
+  
+  response.render('test', { 
+    title: 'Node Test',
+  });
+  
+});
 
-Sys.puts( 'Server running at http://localhost:' + config["frontend_port"] + '/' );
+App.get('/socketio-test', function(request, response) {
 
-/* MongoDB */
+  response.render('socket-index', {
+    title: 'Socket.IO Test',
+  });
 
+});
+
+
+/* == START =================================== */
+
+App.listen( config["frontend_port"], function() {
+  var addr = App.address();
+  console.log( 'Server running at '.green + ('http://' + addr.address + ':' + addr.port + '/').yellow );
+});
+
+
+/* == MONGODB =================================== */
+/*
 DbServer.open(function(err, pClient) {
   DbServer.collection('test_insert', insertData);
   DbServer.collection('test_insert', removeData);
+});
+*/
+
+/* == SOCKET.IO =================================== */
+
+
+Io.sockets.on('connection', function (socket) {
+	// spit out some data on connect
+  socket.emit('news', { hello: 'world!!' });
+	
+	// use an event
+  socket.on('some event', function (data) {
+		console.log(data);
+  });
+
+	socket.on('test form', function(data) {
+		DbServer.open(function(err, pClient) {
+			DbServer.collection('test', function(err, collection) {
+				console.log(data.form);
+				console.log('record shit to db?');
+				collection.insert({name: data.form});
+			});
+			//DbServer.collection('test_insert', removeData);
+		});
+	});
 });
 
 
